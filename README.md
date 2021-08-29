847823bde156de2f3905114bce3cd2ef0131ac02
sharad <sharad_singh@trisysit.com>
2021-08-29 11:40:52 +0530

    task 2 update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Automation_Project
+
+Here we define the bucket name
+S3_bucket=assignmetupgradesharad
+
+
+this command is use to update the packages
+sudo apt update -y
+
+#Then checking if apache is installed or not, and if not install it automatically 
+
+
+#Then installing net-tools so that it can be used for the next operation
+
+then The next step i have done is  checking if apache service is running or not, if not running then restart it
+
+
+
+then we have enabled apache restart automatically
+
+then apache Archiving Logs
+
+then copy to S3 since we have already setup  aws cli on ec2 instance by using commands 
+sudo apt update
+sudo apt install awscli
+and then configured there with the iam role keys
+
+Now we have to shedule the script in crontab so for that run the following cammand
+
+sudo crontab -e
+# here we will enter our script shedule time of execution
+* * * * * sh /root/Automation_Project/automation.sh
+
+# this will execute the script every minute
---
+++ automation.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+S3_bucket=assignmetupgradesharad
+
+sudo apt update -y
+
+#checking if apache is installed or not, and if not install it automatically
+
+if dpkg --get-selections | grep apache | awk '{ print $2 }' | uniq eq 'install'
+then
+echo "installed already"
+else
+
+ echo y | sudo apt install apache2
+
+fi
+
+#installing net-tools so that it can be used for the next operation
+
+echo y | sudo apt-get install net-tools
+
+# checking if apache service is running or not, if not running then restart it
+
+if sudo netstat -tnlp | grep apache | awk '{ print $6 }' | uniq  eq 'LISTEN'
+then
+echo "service running"
+  else
+  sudo systemctl restart apache2
+fi
+
+# enabling apache restart automatically
+
+sudo systemctl enable apache2
+
+#Archiving Logs
+
+for file in $(ls /var/log/apache2/*.$(date '+%d%m%Y-%H%M%S').access.log); do
+    gzip $file
+    mv $file.gz /temp/Sharad-httpd-logs-$(date '+%d%m%Y-%H%M%S').gz
+    aws s3 \
+cp /tmp/Sharad-httpd-logs-$(date '+%d%m%Y-%H%M%S').gz \
+s3://${s3_bucket}/Sharad-httpd-logs-$(date '+%d%m%Y-%H%M%S').gz
+#now copied to S3 since your aws cli is already configured on ec2 instance
+done;
+
+for file in $(ls /var/log/apache2/*.$(date '+%d%m%Y-%H%M%S').error.log); do
+    gzip $file
+    mv $file.gz /temp/Sharad-error-logs-$(date '+%d%m%Y-%H%M%S').gz
+    aws s3 \
+cp /tmp/Sharad-error-logs-$(date '+%d%m%Y-%H%M%S').gz \
+s3://${s3_bucket}/Sharad-error-logs-$(date '+%d%m%Y-%H%M%S').gz
+#now copied to S3 since your aws cli is already configured on ec2 instance
+
+done;
+
+
+# we will shedule this in crontab . Instruction is written in readme file
